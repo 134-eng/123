93f72f07c598c0db20bae05c9767c1ff4dcf932f
134-eng <doriko1402@gmail.com>
2024-11-04 17:17:33 +0200

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,10 @@
+git clone <https://github.com/134-eng/123>
+cd <123>
+python -m venv venv
+venv\Scripts\activate
+
+print("привіт викладач")
+git add <123>
+git commit -m "додано привітання до викладача"
+git push origin main
+
